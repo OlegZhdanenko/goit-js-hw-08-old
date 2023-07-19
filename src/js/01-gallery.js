86b87b92b272d4b1c5459d4f12db150cf87951ea
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2023-07-19 20:43:17 +0300

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -4,10 +4,7 @@ import { galleryItems } from './gallery-items.js';
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
 console.log(galleryItems);
-new SimpleLightbox('.gallery a', {
-    captionsData: 'alt',
-    captionDelay: 250,
-});
+
 const ellGallery = document.querySelector('.gallery')
 const gallaryNew = galleryItems.map(({ original, preview, description }) => `<li class="gallery__item">
   <a class="gallery__link" href="${original}">
@@ -22,16 +19,7 @@ const gallaryNew = galleryItems.map(({ original, preview, description }) => `<li
     .join('');
 
 ellGallery.innerHTML = gallaryNew;
-ellGallery.addEventListener("click", clickOn);
-function clickOn(event) {
-
-  event.preventDefault();
-  
-  
-
-  ellGallery.addEventListener("keyup", event => {
-    if (event.code === "Escape") {
-      instance.close();
-    }
-  })
-}
+new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,12 @@
-import Player from '@vimeo/player';
+import Player from '@vimeo/player'
+import throttle from 'lodash.throttle';
 
-const player = new Player('handstick', {
-    id: 'vimeo-player',
-    width: 640
-});
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-player.on('play', function() {
-    console.log('played the video!');
-});
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay({ seconds }) {
+  localStorage.setItem('videoplayer-current-time', seconds);
+}
+
