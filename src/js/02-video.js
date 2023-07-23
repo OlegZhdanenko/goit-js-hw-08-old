aaa30ec2e0367c0402f479b98526536cbfe47f55
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2023-07-23 14:33:05 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,24 @@
-import Player from '@vimeo/player'
+import player from '@vimeo/player'
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Player(iframe);
-
-player.on('timeupdate', throttle(onPlay, 1000));
+const player = new player(iframe);
 
 function onPlay({ seconds }) {
   localStorage.setItem('videoplayer-current-time', seconds);
 }
+const onPlay = timeupdate (seconds) { }
+player.on('timeupdate', throttle(onPlay, 1000));
 
+player.setCurrentTime(onPlay).then(function(seconds) {
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
