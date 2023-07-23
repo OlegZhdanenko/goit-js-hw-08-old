f44ebe7b147d0cd164ba699da3f1b74809f07302
OlegZhdanenko <125878669+OlegZhdanenko@users.noreply.github.com>
2023-07-23 20:54:05 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,8 +4,12 @@ import throttle from 'lodash.throttle';
  const iframe = document.querySelector('iframe');
  const player = new Player(iframe);
 
-
- player.on('timeupdate', throttle(onPlay, 1000));
+ const onPlay = function (seconds) {
+     localStorage.setItem('videoplayer-current-time',seconds);
+ };
+ 
+player.on('timeupdate', throttle(onPlay, 1000));
+ 
  const currentTime = Number(localStorage.getItem('videoplayer-current-time'));
 
 player.setCurrentTime(currentTime).then(function(seconds) {
